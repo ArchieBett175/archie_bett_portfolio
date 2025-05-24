1393559ef20490ecd0753e47ea8baad11c8670f3
Archiebett <archiebett@hotmail.co.uk>
2025-05-24 13:20:24 +0100

    created contact keys in keyboard style
    created a keyboard style button for my contact links

--- portfolio-site/src/App.jsx
+++ portfolio-site/src/App.jsx
@@ -1,15 +1,22 @@
 import { IoHome, IoDocument } from "react-icons/io5";
 import { FaInfoCircle } from "react-icons/fa";
 import { FaBriefcase } from "react-icons/fa6";
-import Tooltip from "./components/Tooltip/Tooltip";
+import { FiGithub, FiLinkedin } from "react-icons/fi";
+import { PiReadCvLogoBold } from "react-icons/pi";
+import { MdOutlineMailOutline } from "react-icons/md";
 import { AnimatePresence, motion } from "motion/react";
-import { useState } from "react";
+import { use, useState } from "react";
 
 function App() {
   const [showHomeTooltip, setShowHomeTooltip] = useState(false);
   const [showAboutTooltip, setShowAboutTooltip] = useState(false);
   const [showProjTooltip, setShowProjTooltip] = useState(false);
   const [showBlogTooltip, setShowBlogTooltip] = useState(false);
+
+  const [showGitTooltip, setShowGitTooltip] = useState(false);
+  const [showLinkdinTooltip, setShowLinkdinTooltip] = useState(false);
+  const [showMailTooltip, setShowMailTooltip] = useState(false);
+  const [showCVTooltip, setShowCVTooltip] =useState(false);
 
   return (
     <>
@@ -43,6 +50,7 @@ function App() {
                   whileHover={{ scale: 1.4 }}
                   whileTap={{ scale: 0.95 }}
                   transition={{ duration: 0.3 }}
+                  className="cursor-pointer"
                 >
                   <IoHome className="size-7 text-white" />
                 </motion.div>
@@ -70,6 +78,7 @@ function App() {
                   whileHover={{ scale: 1.4 }}
                   whileTap={{ scale: 0.95 }}
                   transition={{ duration: 0.3 }}
+                  className="cursor-pointer"
                 >
                   <FaInfoCircle className="size-7 text-white" />
                 </motion.div>
@@ -97,13 +106,14 @@ function App() {
                   whileHover={{ scale: 1.4 }}
                   whileTap={{ scale: 0.95 }}
                   transition={{ duration: 0.3 }}
+                  className="cursor-pointer"
                 >
                   <FaBriefcase className="size-7 text-white " />
                 </motion.div>
               </li>
             </div>
             <div className="blogLink">
-            <AnimatePresence>
+              <AnimatePresence>
                 {showBlogTooltip && (
                   <motion.p
                     initial={{ opacity: 0 }}
@@ -124,6 +134,7 @@ function App() {
                   whileHover={{ scale: 1.4 }}
                   whileTap={{ scale: 0.95 }}
                   transition={{ duration: 0.3 }}
+                  className="cursor-pointer"
                 >
                   <IoDocument className="size-7 text-white " />
                 </motion.div>
@@ -147,6 +158,142 @@ function App() {
             <p className="font-roboto opacity-40 mt-5">
               Recent Interactive Media Graduate Based In London, UK
             </p>
+            <div className="mt-12">
+              <h1 className="font-roboto font-light opacity-40">
+                Let's Connect ...
+              </h1>
+              <ul className="flex gap-3 mt-2">
+                <div className="rotate-x-30 gitHubBut">
+                  <AnimatePresence>
+                    {showGitTooltip && (
+                      <motion.p
+                        initial={{ opacity: 0 }}
+                        animate={{ y: "-75%", opacity: 1 }}
+                        exit={{ y: "0%", opacity: 0 }}
+                        transition={{ duration: 0.3, ease: "easeInOut" }}
+                        className="absolute text-black bg-white rounded-3xl 
+                        px-3 py-1 font-roboto text-center top-[-20px] ml-[-5px]"
+                      >
+                        Github
+                      </motion.p>
+                    )}
+                  </AnimatePresence>
+                  <li
+                    className="h-15 w-15 cursor-pointer select-none rounded-lg
+                    border-b-2 border-zinc-700 bg-zinc-600 transition-all 
+                    duration-150 [box-shadow:0_10px_0_0_#414145] active:translate-y-2
+                    active:border-b-0 active:[box-shadow:0_0px_0_0_#414145, 0_0px_0_0_#38383d]"
+                    onMouseEnter={() => setShowGitTooltip(true)}
+                    onMouseLeave={() => setShowGitTooltip(false)}
+                  >
+                    <span
+                      className="flex h-full flex-col items-center justify-center
+                      text-lg font-medium text-gray-100"
+                    >
+                      <FiGithub className="size-10" />
+                    </span>
+                  </li>
+                </div>
+                <div className="rotate-x-30 linkedInBut">
+                  <AnimatePresence>
+                    {showLinkdinTooltip && (
+                      <motion.p
+                        initial={{ opacity: 0 }}
+                        animate={{ y: "-75%", opacity: 1 }}
+                        exit={{ y: "0%", opacity: 0 }}
+                        transition={{ duration: 0.3, ease: "easeInOut" }}
+                        className="absolute bg-white rounded-3xl px-3 py-1 text-black
+                        font-roboto text-center top-[-20px] ml-[-13px]"
+                      >
+                        LinkedIn
+                      </motion.p>
+                    )}
+                  </AnimatePresence>
+                  <li
+                    className="h-15 w-15 cursor-pointer select-none rounded-lg
+                    border-b-2 border-zinc-700 bg-zinc-600 transition-all 
+                    duration-150 [box-shadow:0_10px_0_0_#414145] active:translate-y-2
+                    active:border-b-0 active:[box-shadow:0_0px_0_0_#414145, 0_0px_0_0_#38383d]"
+                  
+                    onMouseEnter={()=>setShowLinkdinTooltip(true)}
+                    onMouseLeave={()=>setShowLinkdinTooltip(false)}
+                  
+                  >
+                    <span
+                      className="flex h-full flex-col items-center justify-center
+                      text-lg font-medium text-gray-100"
+                    >
+                      <FiLinkedin className="size-10" />
+                    </span>
+                  </li>
+                </div>
+                <div className="rotate-x-30 mailBut">
+                <AnimatePresence>
+                    {showMailTooltip && (
+                      <motion.p
+                        initial={{ opacity: 0 }}
+                        animate={{ y: "-75%", opacity: 1 }}
+                        exit={{ y: "0%", opacity: 0 }}
+                        transition={{ duration: 0.3, ease: "easeInOut" }}
+                        className="absolute bg-white rounded-3xl px-3 py-1 text-black
+                        font-roboto text-center top-[-20px] ml-[0]"
+                      >
+                        Email
+                      </motion.p>
+                    )}
+                  </AnimatePresence>
+                  <li
+                    className="h-15 w-15 cursor-pointer select-none rounded-lg
+                    border-b-2 border-zinc-700 bg-zinc-600 transition-all 
+                    duration-150 [box-shadow:0_10px_0_0_#414145] active:translate-y-2
+                    active:border-b-0 active:[box-shadow:0_0px_0_0_#414145, 0_0px_0_0_#38383d]"
+                  
+                    onMouseEnter={() => setShowMailTooltip(true)}
+                    onMouseLeave={() => setShowMailTooltip(false)}
+                  >
+                    <span
+                      className="flex h-full flex-col items-center justify-center
+                      text-lg font-medium text-gray-100"
+                    >
+                      <MdOutlineMailOutline className="size-10" />
+                    </span>
+                  </li>
+                </div>
+                <div className="rotate-x-30 cvBut">
+                <AnimatePresence>
+                    {showCVTooltip && (
+                      <motion.p
+                        initial={{ opacity: 0 }}
+                        animate={{ y: "-75%", opacity: 1 }}
+                        exit={{ y: "0%", opacity: 0 }}
+                        transition={{ duration: 0.3, ease: "easeInOut" }}
+                        className="absolute bg-white rounded-3xl px-3 py-1 text-black
+                        font-roboto text-center top-[-20px] ml-[7px]"
+                      >
+                        CV
+                      </motion.p>
+                    )}
+                  </AnimatePresence>
+                  <li
+                    className="h-15 w-15 cursor-pointer select-none rounded-lg
+                    border-b-2 border-zinc-700 bg-zinc-600 transition-all 
+                    duration-150 [box-shadow:0_10px_0_0_#414145] active:translate-y-2
+                    active:border-b-0 active:[box-shadow:0_0px_0_0_#414145, 0_0px_0_0_#38383d]"
+                  
+                    onMouseEnter={()=>setShowCVTooltip(true)}
+                    onMouseLeave={()=>setShowCVTooltip(false)}
+                  
+                  >
+                    <span
+                      className="flex h-full flex-col items-center justify-center
+                      text-lg font-medium text-gray-100"
+                    >
+                      <PiReadCvLogoBold className="size-10" />
+                    </span>
+                  </li>
+                </div>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
