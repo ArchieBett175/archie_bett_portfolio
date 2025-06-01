48bdf1a649344e43f037447a381b9b6b083a1d8b
Archiebett <archiebett@hotmail.co.uk>
2025-06-01 22:45:25 +0100

    Finished the final layout and responsiveness of bento grid
    from large medium and small, wanted to mess about with aspect ratio and try to get a smooth transition. But thats for another day. Essentially all tailwind today to try and get the parts moving at the correct breakpoints.

--- portfolio-site/src/App.jsx
+++ portfolio-site/src/App.jsx
@@ -503,59 +503,59 @@ function App() {
             <div className="w-full h-1 bg-neutral-500 -mt-3"></div>
           </div>
         </div>
+        <div className="p-10 bg-black"></div>
         {/* Div for Bento Grid */}
-        <div className="h-screen w-full flex items-center justify-center bg-black">
+        <div className="h-500 w-full flex items-center justify-center bg-black md:h-screen">
           <div
-            className=" w-[1164px] auto-rows-[minmax(0,1fr)]
+            className="w-[1164px] h-full   auto-rows-[minmax(0,1fr)]
               grid-cols-10 gap-3 p-10
               flex-col 
               max-lg:grid-cols-8
-              md:grid max-md:flex max-md:gap-4 aspect-12/10
+              md:grid max-md:flex max-md:gap-3
               "
           >
             <div
               className="col-start-1 col-end-4 row-start-1 row-end-4 bg-red-500 
-            rounded-3xl aspect-square"
+            rounded-xl max-md:h-125"
             ></div>
             <div
               className="col-start-1 col-end-4 row-start-4 row-end-5
-            rounded-3xl grid grid-cols-3 gap-3 "
+            rounded-xl grid grid-cols-3 gap-3 max-md:h-50"
             >
-              <div className="col-span-1 bg-blue-400 rounded-3xl aspect-square"></div>
-              <div className="col-span-1 bg-blue-500 rounded-3xl aspect-square"></div>
-              <div className="col-span-1 bg-blue-600 rounded-3xl aspect-square"></div>
+              <div className="col-span-1 bg-blue-400 rounded-xl"></div>
+              <div className="col-span-1 bg-blue-500 rounded-xl"></div>
+              <div className="col-span-1 bg-blue-600 rounded-xl"></div>
             </div>
             <div
               className="col-start-6 col-end-11 row-start-1 row-end-4 bg-amber-700 
-            rounded-3xl max-lg:col-start-4 max-lg:col-end-9 aspect-12/7"
+            rounded-xl max-lg:col-start-4 max-lg:col-end-9 max-md:h-65"
             ></div>
             <div
               className="col-start-6 col-end-11 row-start-4 row-end-5 bg-stone-700 
-            rounded-3xl max-lg:col-start-4 max-lg:col-end-9 aspect-22/4"
+            rounded-xl max-lg:col-start-4 max-lg:col-end-9 max-md:h-30"
             ></div>
             <div
               className="col-start-4 col-end-6 row-start-1 row-end-3 bg-purple-600 
-            rounded-3xl max-lg:col-start-1 max-lg:col-end-5 max-lg:row-start-5 
-            max-lg:row-end-7 lg:aspect-square aspect-19/9"
+            rounded-xl max-lg:col-start-1 max-lg:col-end-5 max-lg:row-start-5 
+            max-lg:row-end-7 max-md:h-30"
             ></div>
             <div
               className="col-start-4 col-end-6 row-start-3 row-end-5 bg-slate-700
-            rounded-3xl max-lg:col-start-5 max-lg:col-end-9 max-lg:row-start-8 
-            max-lg:row-end-10 lg:aspect-square aspect-19/9"
+            rounded-xl max-lg:col-start-5 max-lg:col-end-9 max-lg:row-start-8 
+            max-lg:row-end-10 max-md:h-30"
             ></div>
             <div
               className="col-start-1 col-end-6 row-start-5 row-end-8 bg-green-500 
-            rounded-3xl max-lg:col-end-5 max-lg:row-start-7 max-lg:row-end-10
-            aspect-19/11
+            rounded-xl max-lg:col-end-5 max-lg:row-start-7 max-lg:row-end-10 max-md:h-65
             "
             ></div>
             <div
               className="col-start-6 col-end-11 row-start-5 row-end-8 bg-pink-700 
-            rounded-3xl max-lg:col-start-5 max-lg:col-end-9 aspect-19/11"
+            rounded-xl max-lg:col-start-5 max-lg:col-end-9 max-md:h-65"
             ></div>
             <div
               className="col-start-1 col-end-11 row-start-8 row-end-9 bg-cyan-700 
-            rounded-3xl max-lg:col-end-9 max-lg:row-start-10 max-lg:row-end-11 aspect-45/4"
+            rounded-xl max-lg:col-end-9 max-lg:row-start-10 max-lg:row-end-11 max-md:h-25"
             ></div>
           </div>
         </div>
